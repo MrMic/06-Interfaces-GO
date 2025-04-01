8fc8b8ac5d464ddcc8b3e7e58f5abdfd346b0215
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-01 14:45:47 +0200

    feat: Implementation of io.Copy()

--- HTTP/http.go
+++ HTTP/http.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,9 +16,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999) // 99kb
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	io.Copy(os.Stdout, resp.Body)
 
 	defer resp.Body.Close()
+}
+
+// ______________________________________________________________________
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
